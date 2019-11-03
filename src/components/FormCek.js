4602bf29b4d515682675faf514651b0dd01c68ed
Agung Setyo Nugroho <agungsn1994@gmail.com>
2019-11-03 13:19:49 +0700

    update autosuggest

--- src/components/FormCek.js
+++ src/components/FormCek.js
@@ -1,45 +1,46 @@
 import React, { Component } from 'react';
 import '../styles/FormCek.css';
 import Autosuggest from 'react-autosuggest';
+import axios from 'axios';
+import {cities} from '../assets/Cities';
+//
 
-const languages = [
-    {
-        name: 'C',
-        year: 1972
-    },
-    {
-        name: 'crr',
-        year: 2012
-    },
-    {
-        name: 'crrsdfsd',
-        year: 2012
-    }];
+const instanceAxios = axios.create({
+    baseURL: 'https://api.rajaongkir.com/',
+    mode: 'no-cors',
+    headers: {
+        'key': 'a92bf940de73d54372d421393620c4da',
+    }
+  });
 
 const getSuggestions = value => {
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
 
-    return inputLength === 0 ? [] : languages.filter(lang =>
-        lang.name.toLowerCase().slice(0, inputLength) === inputValue
+    return inputLength === 0 ? [] : cities.filter(lang =>
+        lang.city_name.toLowerCase().slice(0, inputLength) === inputValue
     );
 };
 
-const getSuggestionValue = suggestion => suggestion.name;
+const getSuggestionValue = suggestion => suggestion.city_name;
 
 const renderSuggestion = suggestion => (
     <div>
-        {suggestion.name}
+        {suggestion.city_name}
     </div>
 );
 
 
 class FormCek extends Component {
     state = {
+        semuaKota:[],
         kotaAsal: '',
-        KotaTujuan: '',
+        objKotaAsal:{},
+        kotaTujuan: '',
+        objKotaTujuan:{},
         suggestionsKotaAsal: [],
-        suggestionsKotaTujuan: []
+        suggestionsKotaTujuan: [],
+        berat:1
     };
 
 
@@ -50,9 +51,12 @@ class FormCek extends Component {
     };
     onChangeKotaTujuan = (event, { newValue }) => {
         this.setState({
-            KotaTujuan: newValue
+            kotaTujuan: newValue
         });
     };
+    onChangeBerat=(event)=>{
+        this.setState({berat:event.target.value})
+    }
 
     onSuggestionsFetchRequestedKotaAsal = ({ value }) => {
         this.setState({
@@ -78,20 +82,54 @@ class FormCek extends Component {
         });
     };
 
+    onSuggestionKotaAsalSelected=(event, { suggestion})=>{
+        this.setState({
+            objKotaAsal:suggestion
+        })
+    }
+
+    onSuggestionKotaTujuanSelected=(event, { suggestion})=>{
+        this.setState({
+            objKotaTujuan:suggestion
+        })
+    }
+
+    componentDidMount(){
+        instanceAxios.get('/starter/city')
+            .then(function (response) {console.log(response);
+        })
+    }
+
+    cekOngkir=(idKotaAsal,idKotaTujuan,weight,courier)=>{
+        instanceAxios.post('/starter/city',{
+            origin: idKotaAsal,
+            destination: idKotaTujuan,
+            weight: weight,
+            courier: courier
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+    }
+
     render() {
-        const { kotaAsal, kotaTujuan, suggestionsKotaAsal, suggestionsKotaTujuan } = this.state;
+        const { kotaAsal, kotaTujuan, suggestionsKotaAsal, suggestionsKotaTujuan, objKotaAsal, objKotaTujuan,berat } = this.state;
+        console.log(objKotaTujuan);
 
         return (
-            <div>
-                <h1 class="headline">Cek Ongkir</h1>
+            <div class="flex h-screen">
+            <div class="w-2/5 bg-blue-400"></div>
+            <div class="w-3/5 p-20">
+                <h1 class="text-5xl font-bold headline">Cek Ongkir</h1>
                 <p>Seluruh kota di Indonesia tanpa perlu input captcha </p>
-                <div>
+                <div class="mt-8">
                     <p class="headline">Kota Asal</p>
                     <Autosuggest
                         suggestions={suggestionsKotaAsal}
                         onSuggestionsFetchRequested={this.onSuggestionsFetchRequestedKotaAsal}
                         onSuggestionsClearRequested={this.onSuggestionsClearRequestedKotaAsal}
                         getSuggestionValue={getSuggestionValue}
+                        onSuggestionSelected={this.onSuggestionKotaAsalSelected}
                         renderSuggestion={renderSuggestion}
                         inputProps={{
                             placeholder: 'Kota asal pengiriman',
@@ -100,7 +138,7 @@ class FormCek extends Component {
                         }}
                     />
                 </div>
-                {/* <div>
+                <div class="mt-6">
                     <p class="headline">Kota Tujuan</p>
                     <Autosuggest
                         suggestions={suggestionsKotaTujuan}
@@ -108,16 +146,27 @@ class FormCek extends Component {
                         onSuggestionsClearRequested={this.onSuggestionsClearRequestedKotaTujuan}
                         getSuggestionValue={getSuggestionValue}
                         renderSuggestion={renderSuggestion}
+                        onSuggestionSelected={this.onSuggestionKotaTujuanSelected}
                         inputProps={{
                             placeholder: 'Kota tujuan pengiriman',
                             value: kotaTujuan,
                             onChange: this.onChangeKotaTujuan
                         }}
                     />
-                </div> */}
+                </div>
+                <div class="mt-6">
+                    <p class="headline">Berat</p>
+                    <input class="w-8 bg-gray-200" type="text" name="berat" value={berat} onChange={this.onChangeBerat}/> kg
+                </div>
 
-                <button type="button" class="headline bg-blue-600 text-white w-1/4">Cek Ongkir</button>
-
+                <button 
+                type="button" 
+                class="headline bg-blue-600 text-white w-1/4 mt-8 p-2 rounded-full"
+                onClick={()=>{
+                    this.cekOngkir(objKotaAsal.city_id,objKotaTujuan.city_id,berat*1000,"jne")
+                }}
+                >Cek Ongkir</button>
+            </div>
             </div>)
     }
 
